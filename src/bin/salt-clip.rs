e0b1327c6d21fed663dff737834bf21982841bd2
M@ Dunlap <mattdunlap+git@gmail.com>
2022-03-24 15:29:43 -0700

    added an env-args argument to pass through to clippy

--- src/bin/salt-clip.rs
+++ src/bin/salt-clip.rs
@@ -15,16 +15,38 @@ use regex::Regex;
 
 use salt_spray::find_manifest;
 
+static CLIPPY_ENV_ARGS: &str = "--env-args=";
 static CLIPPY_FILE_IDENTIFICATION: Lazy<Regex> = Lazy::new(|| {
     // The filename ends up in capture group #1
     Regex::new(r"-->\s+([^:]+)").unwrap()
 });
 
+/// Parses `args` and loads them into the pending command.
+fn load_env_args(cmd: &mut Command, args: &Option<String>) {
+    if let Some(args) = args {
+        for assignment in args.split(';') {
+            if let Some((name, val)) = assignment.split_once('=') {
+                cmd.env(name, val);
+            }
+        }
+    }
+}
+
 /// Runs Clippy on a crate, but only outputs lints for files in the given set.
-fn lint_crate(cargo_toml: &str, files: &BTreeSet<String>) -> i32 {
+fn lint_crate(cargo_toml: &str, files: &BTreeSet<String>, args: &Option<String>) -> i32 {
+
+
+
+
+    // TODO: remove extra eprintlns and set a sane default for result
+
+
+
+
     let mut result = 5;
     // let mut result = 0;
     let mut cmd = Command::new("cargo");
+    load_env_args(&mut cmd, args);
     cmd.args(["clippy", "--no-deps", "--quiet", "--manifest-path", cargo_toml]);
 
     match cmd.output() {
@@ -40,8 +62,6 @@ fn lint_crate(cargo_toml: &str, files: &BTreeSet<String>) -> i32 {
                     } else {
                         eprintln!("None of the files ended with {}", project_relative_filename);
                     }
-                } else {
-                    eprintln!("=======\nBad Input\n{}", found_lint);
                 }
             }
         }
@@ -55,21 +75,28 @@ fn main() {
     let mut args = env::args();
     drop(args.next());
 
+    let mut clippy_env_args = None;
+
     // Clippy can only operate on whole crates at a time, so rather than lint
     // each crate for each file within it, we group the file names first and
     // only run clippy once for each crate.
     let mut files_by_crate: HashMap<String, BTreeSet<String>> = HashMap::new();
-    for file in args {
-        if let Some(manifest_path) = find_manifest(&file) {
+    for mut arg in args {
+        if arg.starts_with(CLIPPY_ENV_ARGS) {
+            clippy_env_args = Some(arg.split_off(CLIPPY_ENV_ARGS.len()));
+        } else if let Some(manifest_path) = find_manifest(&arg) {
             let manifest_path = manifest_path.to_string_lossy().to_string();
             let files = files_by_crate.entry(manifest_path).or_default();
-            files.insert(file);
+            files.insert(arg);
         }
     }
 
+    // pre-commit looks for either changes to files or our return code to indicate
+    // that the commit should not be allowed.  Since we can't automatically fix
+    // issues, we instead exit with the violation count.
     let mut violation_count = 0;
     for (cargo_toml, files) in files_by_crate.iter() {
-        violation_count += lint_crate(cargo_toml, files);
+        violation_count += lint_crate(cargo_toml, files, &clippy_env_args);
     }
     process::exit(violation_count);
 }
